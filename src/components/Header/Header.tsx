9e0599a905c7d97b86fd780b4afad527638c40ce
Andrew <asoborev@mail.ru>
2024-02-27 19:27:02 +0300

    Реализовал компонент Header, добавил index.d.ts для поддердки module.css

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import cn from './header.module.css'
+const Header: FC = () => {
+    return (
+        <header className={cn.header}>
+           
+        </header>
+    )
+}
+
+export default Header
