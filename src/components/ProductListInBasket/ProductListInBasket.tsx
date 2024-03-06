dd947a8a204f87da759d4a4a8b4ffdc09d8db856
Andrew <asoborev@mail.ru>
2024-03-06 19:07:57 +0300

    убрал лишнюю обертку над компонентом

--- src/components/ProductListInBasket/ProductListInBasket.tsx
+++ src/components/ProductListInBasket/ProductListInBasket.tsx
@@ -10,14 +10,12 @@ const ProductListInBasket = () => {
         <div className={cn.productList}>
             {productList.map((product: any) => {
                 return (
-                    <>
-                        <ProductInBasket
-                            key={product.id}
-                            id={product.id}
-                            title={product.title}
-                            urlImg={product.urlImg}
-                            price={product.price}/>
-                    </>
+                    <ProductInBasket
+                        key={product.id}
+                        id={product.id}
+                        title={product.title}
+                        urlImg={product.urlImg}
+                        price={product.price}/>
                 )
             })}
         </div>
